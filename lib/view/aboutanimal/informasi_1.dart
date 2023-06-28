0419be17420aa547fe2f6a5d88ac0bba8d36ba73
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-06-28 15:51:40 +0700

    create view information for user (pawrents) and navigation from about animal to informasi1 page

---
+++ lib/view/aboutanimal/informasi_1.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Informasi1 extends StatelessWidget {
+  const Informasi1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Important Things'),
+          titleTextStyle: GoogleFonts.lato(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        body: SingleChildScrollView(
+            child: SafeArea(
+                child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              'Important Things for Your Pets',
+              style:
+                  GoogleFonts.lato(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Image.asset('assets/images/informasi1.png'),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    spreadRadius: 7,
+                    blurRadius: 10,
+                    offset: const Offset(1, 1),
+                    color: Colors.grey.withOpacity(0.3),
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(15),
+              height: 430,
+              width: 340,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                'Hewan dengan pemiliknya selain memiliki ikatan erat, disertai tanggung jawab yang besar bagi pemilik terhadap hewan peliharaannya. Berikanlah kasih sayang dan perhatian yang penuh bagi hewan dengan melakukan: ',
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                '1. Pemeriksaan kesehatan secara rutin ke\n     dokter hewan untuk mengetahui kondisi\n     kesehatannya.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                '2. Gromming - Terutama bagi hewan \n    berbulu panjang. Anda dapat menyisirkan \n   bulu-bulunya, memandikan dengan \n  shampoo khusus hewan dan pemberian\n   antiparasit secara rutin.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                '3.Sosialisasi - Perkenalkanlah hewan anda\n  dengan lingkungan sekitarnya, baik hewan\n  lain maupun manusia.',
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      textAlign: TextAlign.justify,
+                      text: TextSpan(
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        children: [
+                          TextSpan(
+                            text:
+                                '4. Disiplin - Ajarkan hewan anda apa yang\n     boleh mereka lakukan dan apa yang\n     dilarang bagi mereka.',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+            )
+          ],
+        ))));
+  }
+}
