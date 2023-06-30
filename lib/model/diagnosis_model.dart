4711d6acad81c67314fe19adeeb9590c2947b63d
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-06-30 20:50:35 +0700

    adding feedback in diagnosis model

--- lib/model/diagnosis_model.dart
+++ lib/model/diagnosis_model.dart
@@ -3,22 +3,26 @@ import 'dart:convert';
 class DiagnosisModel {
   String? id;
   String? namaPenyakit;
+  String? feedback;
   DiagnosisModel({
     this.id,
     this.namaPenyakit,
+    this.feedback,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'namaPenyakit': namaPenyakit,
+      'feedback': feedback,
     };
   }
 
   factory DiagnosisModel.fromMap(Map<String, dynamic> map) {
     return DiagnosisModel(
       id: map['id'],
-      namaPenyakit: map['namaPenyakit'] ?? '',
+      namaPenyakit: map['namaPenyakit'],
+      feedback: map['feedback'],
     );
   }
 
