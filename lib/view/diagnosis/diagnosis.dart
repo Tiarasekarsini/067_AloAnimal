02253e99ff078de3a7d4913c060fd5a813bfd384
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-06-30 12:16:06 +0700

    update view for diagnosis

--- lib/view/diagnosis/diagnosis.dart
+++ lib/view/diagnosis/diagnosis.dart
@@ -1,3 +1,5 @@
+import 'package:aloanimal/view/diagnosis/riwayat_diagnosis.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,91 +12,227 @@ class Diagnosis extends StatefulWidget {
 
 class _DiagnosisState extends State<Diagnosis> {
   String? namaPenyakit;
-  List<bool> checkedList = [false, false, false];
+  List<bool> checkedList = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Disease Diagnosis'),
-        titleTextStyle: GoogleFonts.lato(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
+        appBar: AppBar(
+          title: const Text('Disease Diagnosis'),
+          titleTextStyle: GoogleFonts.lato(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
         ),
-      ),
-      body: ListView(children: [
-        Column(children: [
-          Container(
-            margin: const EdgeInsets.only(left: 25, right: 25, top: 10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  spreadRadius: 7,
-                  blurRadius: 10,
-                  offset: const Offset(1, 1),
-                  color: Colors.grey.withOpacity(0.3),
+        body: SingleChildScrollView(
+            child: Container(
+          margin:
+              const EdgeInsets.only(left: 25, right: 25, top: 20, bottom: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                spreadRadius: 7,
+                blurRadius: 10,
+                offset: const Offset(1, 1),
+                color: Colors.grey.withOpacity(0.3),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(15),
+          height: 900,
+          width: 360,
+          child: ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: GoogleFonts.lato(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Pawrents!',
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            padding: const EdgeInsets.all(15),
-            height: 1300,
-            width: 360,
-            child: Column(
-              children: [
-                CheckboxListTile(
-                    title: Text('Demam'),
-                    value: checkedList[0],
-                    onChanged: (value) {
-                      setState(() {
-                        checkedList[0] = value!;
-                      });
-                    }),
-                CheckboxListTile(
-                  title: Text('Diare'),
-                  value: checkedList[1],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: GoogleFonts.lato(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text:
+                          'Check some of the symptoms experienced by your animal to find out the diagnosis of the disease ',
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                color: Color.fromARGB(158, 255, 64, 128),
+                thickness: 1,
+                indent: 27,
+                endIndent: 27,
+              ),
+              CheckboxListTile(
+                  title: Text('Demam'),
+                  value: checkedList[0],
                   onChanged: (value) {
                     setState(() {
-                      checkedList[1] = value!;
+                      checkedList[0] = value!;
                     });
-                  },
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      if (checkedList[0] && checkedList[1]) {
-                        namaPenyakit = "Demam";
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text('Ini adalah hasil diagnosis:'),
-                                content: Text('Nama Penyakit: $namaPenyakit'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                        backgroundColor: Colors.blue),
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text(
-                                      'Save',
-                                      style: GoogleFonts.lato(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  )
-                                ],
-                              );
-                            });
-                      }
-                    },
-                    child: Text('Submit'))
-              ],
-            ),
-          )
-        ])
-      ]),
-    );
+                  }),
+              CheckboxListTile(
+                title: Text('Diare Ringan'),
+                value: checkedList[1],
+                onChanged: (value) {
+                  setState(() {
+                    checkedList[1] = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                  title: Text('Batuk'),
+                  value: checkedList[2],
+                  onChanged: (value) {
+                    setState(() {
+                      checkedList[2] = value!;
+                    });
+                  }),
+              CheckboxListTile(
+                title: Text('Mata Berair'),
+                value: checkedList[3],
+                onChanged: (value) {
+                  setState(() {
+                    checkedList[3] = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                  title: Text('Hidung Berair'),
+                  value: checkedList[4],
+                  onChanged: (value) {
+                    setState(() {
+                      checkedList[4] = value!;
+                    });
+                  }),
+              CheckboxListTile(
+                title: Text('Diare Berdarah'),
+                value: checkedList[5],
+                onChanged: (value) {
+                  setState(() {
+                    checkedList[5] = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                  title: Text('Muntah'),
+                  value: checkedList[6],
+                  onChanged: (value) {
+                    setState(() {
+                      checkedList[6] = value!;
+                    });
+                  }),
+              CheckboxListTile(
+                title: Text('Hilang Nafsu Makan'),
+                value: checkedList[7],
+                onChanged: (value) {
+                  setState(() {
+                    checkedList[7] = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                  title: Text('Kekuningan'),
+                  value: checkedList[8],
+                  onChanged: (value) {
+                    setState(() {
+                      checkedList[8] = value!;
+                    });
+                  }),
+              CheckboxListTile(
+                title: Text('Nyeri Otot'),
+                value: checkedList[9],
+                onChanged: (value) {
+                  setState(() {
+                    checkedList[9] = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                  title: Text('Bersin-bersin'),
+                  value: checkedList[10],
+                  onChanged: (value) {
+                    setState(() {
+                      checkedList[10] = value!;
+                    });
+                  }),
+              CheckboxListTile(
+                title: Text('Dehidrasi'),
+                value: checkedList[11],
+                onChanged: (value) {
+                  setState(() {
+                    checkedList[11] = value!;
+                  });
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (checkedList[0] && checkedList[1]) {
+                    namaPenyakit = "Demam";
+                    AwesomeDialog(
+                      context: context,
+                      dialogType: DialogType.WARNING,
+                      animType: AnimType.TOPSLIDE,
+                      showCloseIcon: true,
+                      title: 'Result',
+                      desc: "Kucing anda terkena penyakit $namaPenyakit",
+                      btnOkOnPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RiwayatDiagnosis()),
+                        );
+                      },
+                      btnOkColor: Colors.blue,
+                    )..show(); // Add .show() to display the AwesomeDialog
+                  }
+                },
+                child: Text('Cek'),
+              ),
+            ],
+          ),
+        )));
   }
 }
